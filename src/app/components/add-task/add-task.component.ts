913951b3dcb22645448bfb91ccc902206d8df5ba
Puren Tap <purentap@gmail.com>
2022-02-03 23:48:07 +0300

    add btn functionality implemented

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Task } from 'src/app/Task';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
@@ -10,7 +13,13 @@ export class AddTaskComponent implements OnInit {
   text: string;
   day: string;
   reminder: boolean = false;
-  constructor() { }
+  showAddTask: boolean;
+  subscription: Subscription;
+  
+  constructor(private uiService:UiService) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
+
+   }
 
   ngOnInit(): void {
   }
